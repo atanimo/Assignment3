03e7ac91c244bf5534f6319806313e762ce2ffdc
atanimo <atanimo@gmail.com>
2013-08-18 21:21:25 -0700

    initial commit

---
+++ CodeFellowsA3.js
@@ -0,0 +1,10 @@
+/*
+(3) Generate a Backbone.js, Ember.js, AND/or Spine.js app that allows a user to create, edit, update, and destroy (delete) users. 
+Users should have first name, last name, and email address fields. If you have time, add extra features to this app (be creative) 
+and tell us what you did so we don’t miss it. Bonus: deploy this app live somewhere so we can interact with it.
+
+
+I spent a lot of time researching this one, and I honestly think it is too difficult for me to do with the knowledge base that I have. Reading
+about backbone has me feeling confused because I don't know why a lot of pieces of code are present, or what their purpose is.  I may be incorrect, 
+but it seems like conceptually some of the code in this program should be similar to that of the previous assignment; such as creating and destroying
+things. Although this project is too difficult for me now, I am eager to learn about the skills required to write this program. */
